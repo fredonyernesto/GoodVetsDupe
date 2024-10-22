b574dfca80d6a7dfcc7d0d5f1852d21dd64f04ff
SHMERNIE <fredony.ernesto@gmail.com>
2024-10-21 20:50:16 -0700

    added a react-dom, router, pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,55 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx'
+import Home from './pages/Home'
+// import Hospitals from './pages/Hospitals';
+// import Locations from './pages/Locations';
+// import Services from './pages/Services';
+// import Wellness from './pages/Wellness';
+// import Join from './pages/Join';
+// import Portal from './pages/Portal';
+// import Book from './pages/Book';
+// import NotFound from './pages/NotFound';
+// import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/hospitals',
+        element: <Hospitals />
+      }, {
+        path: '/locations',
+        element: <Locations />
+      }, {
+        path: '/services',
+        element: <Services />
+      }, {
+        path: '/wellness',
+        element: <Wellness />
+      }, {
+        path: '/join',
+        element: <Join />
+      }, {
+        path: '/portal',
+        element: <Portal />
+      }, {
+        path: '/book',
+        element: <Book />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}></RouterProvider>
 )
